0d99821951603d06447448ddc8e7e391adaf5fd0
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-08-26 09:46:47 +0200

    Legg til Kotlin

---
+++ src/test/java/no/nav/veilarbveileder/controller/VeilederControllerTest.kt
@@ -0,0 +1,12 @@
+package no.nav.veilarbveileder.controller
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class VeilederControllerTest {
+
+    @Test
+    fun `placeholder test`() {
+        assertTrue(true)
+    }
+}
