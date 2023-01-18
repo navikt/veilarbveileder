dc0c0f21b537cc3ed6951c1706fe58ca080ce0cc
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-01-18 13:53:22 +0100

    Fix tests

--- src/test/java/no/nav/veilarbveileder/config/ApplicationTestConfig.kt
+++ src/test/java/no/nav/veilarbveileder/config/ApplicationTestConfig.kt
@@ -2,8 +2,10 @@ package no.nav.veilarbveileder.config
 
 import no.nav.common.client.nom.NomClient
 import no.nav.common.client.nom.NomClientImpl
+import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient
 import no.nav.poao_tilgang.client.PoaoTilgangClient
 import no.nav.veilarbveileder.mock.PoaoTilgangClientMock
+import org.mockito.Mockito
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
@@ -19,5 +21,10 @@ class ApplicationTestConfig {
     @Bean
     fun poaoTilgangClient(): PoaoTilgangClient {
         return PoaoTilgangClientMock()
+    }
+
+    @Bean
+    fun azureAdMachineToMachineTokenClient(): AzureAdMachineToMachineTokenClient {
+        return Mockito.mock(AzureAdMachineToMachineTokenClient::class.java)
     }
 }
