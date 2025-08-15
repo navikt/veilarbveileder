baae92d3e28885fbcc61e294d0daf31b5da223fd
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-08-15 12:52:58 +0200

    Rydde litt

--- src/main/java/no/nav/veilarbveileder/service/EnhetService.kt
+++ src/main/java/no/nav/veilarbveileder/service/EnhetService.kt
@@ -8,6 +8,7 @@ import no.nav.common.client.norg2.Enhet
 import no.nav.common.client.norg2.Norg2Client
 import no.nav.common.types.identer.EnhetId
 import no.nav.common.types.identer.NavIdent
+import no.nav.veilarbveileder.client.MicrosoftGraphClient
 import no.nav.veilarbveileder.domain.PortefoljeEnhet
 import no.nav.veilarbveileder.utils.Mappers
 import org.slf4j.LoggerFactory
@@ -18,16 +19,16 @@ import java.util.stream.Collectors
 @Service
 @RequiredArgsConstructor
 @Slf4j
-class EnhetService {
+class EnhetService(
+    private val norg2Client: Norg2Client,
+    private val axsysClient: AxsysClient
+) {
     val logger = LoggerFactory.getLogger(javaClass)
 
-    private val norg2Client: Norg2Client? = null
-
-    private val axsysClient: AxsysClient? = null
 
     fun hentEnhet(enhetId: EnhetId): Optional<PortefoljeEnhet> {
         try {
-            val enhet = norg2Client!!.hentEnhet(enhetId.get())
+            val enhet = norg2Client.hentEnhet(enhetId.get())
             return Optional.of(Mappers.tilPortefoljeEnhet(enhet))
         } catch (e: Exception) {
             logger.warn(String.format("Fant ikke enhet med id %s", enhetId), e)
@@ -35,22 +36,20 @@ class EnhetService {
         }
     }
 
-    fun alleEnheter(): MutableList<PortefoljeEnhet?> {
-        return norg2Client!!.alleAktiveEnheter()
+    fun alleEnheter(): List<PortefoljeEnhet?> {
+        return norg2Client.alleAktiveEnheter()
             .stream()
             .map { enhet: Enhet? -> Mappers.tilPortefoljeEnhet(enhet) }
             .collect(Collectors.toList())
     }
 
-    fun veilederePaEnhet(enhetId: EnhetId?): MutableList<NavIdent?>? {
-        return axsysClient!!.hentAnsatte(enhetId)
+    fun veilederePaEnhet(enhetId: EnhetId?): List<NavIdent?>? {
+        return axsysClient.hentAnsatte(enhetId)
     }
 
-    fun hentTilganger(navIdent: NavIdent?): MutableList<PortefoljeEnhet?> {
-        return axsysClient!!.hentTilganger(navIdent)
-            .stream()
+    fun hentTilganger(navIdent: NavIdent?): List<PortefoljeEnhet?> {
+        return axsysClient.hentTilganger(navIdent)
             .map { axsysEnhet: AxsysEnhet? -> Mappers.tilPortefoljeEnhet(axsysEnhet) }
-            .collect(Collectors.toList())
     }
 }
 
